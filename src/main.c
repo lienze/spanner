84a019fa95f06c140e44b0b05a9fff243b85ca20
Enze Li <enze.li@hotmail.com>
2022-07-03 22:55:02 +0800

    main: add usage function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+static void usage()
+{
+  FILE *out = stdout;
+  fputs("Spanner for Kernel Development.\n", out);
+
+  fputs("-h, --h         Show this help info.\n"
+	"-v, --version   Show version info.\n",
+	out);
+
+  exit(0);
+}
 
 int main()
 {
-  printf("Hello spanner\n");
+  /* printf("Hello spanner\n"); */
+  usage();
   return 0;
 }
